eafbceaab0c4b4e5b1328c1ad6aee3e4313f12ee
Diego Garcia <dgarciacosio@gmail.com>
2020-08-09 13:15:13 +0200

    Añadidos algoritmos y parte de la representación

---
+++ run.py
@@ -0,0 +1,23 @@
+from algorithms import bubble_sort, insertion_sort, merge_sort, quick_sort, selection_sort
+
+
+
+data = [3,2,1,0,-1,-2,-3]
+print("bubble sort\n")
+bubble_sort(data)
+input()
+data = [3,2,1,0,-1,-2,-3]
+print("insertion sort\n")
+insertion_sort(data)
+input()
+data = [3,2,1,0,-1,-2,-3]
+print("merge sort\n")
+merge_sort(data)
+input()
+data = [3,2,1,0,-1,-2,-3]
+print("quick sort\n")
+quick_sort(data)
+input()
+data = [3,2,1,0,-1,-2,-3]
+print("selection sort\n")
+selection_sort(data)
